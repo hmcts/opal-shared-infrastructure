22bd5e491771ddda35f26ea3be1fee78f64f0bfb
Dan Lysiak <50049163+danlysiak@users.noreply.github.com>
2023-11-24 10:36:18 +0000

    Update app-insights.tf

--- app-insights.tf
+++ app-insights.tf
@@ -19,3 +19,9 @@ resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   value        = azurerm_application_insights.appinsights.connection_string
   key_vault_id = module.opal_key_vault.key_vault_id
 }
+
+resource "azurerm_key_vault_secret" "azure_appinsights_key" {
+  name         = "AppInsightsInstrumentationKey"
+  value        = azurerm_application_insights.appinsights.instrumentation_key
+  key_vault_id = module.opal_key_vault.key_vault_id
+}
