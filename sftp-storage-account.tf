de7c692b513c8b07e075b8948385c0f54ff65471
Sabah Irfan <sabah.irfan@gmail.com>
2024-04-09 16:46:15 +0100

    PO-267: add sftp_local_user

--- sftp-storage-account.tf
+++ sftp-storage-account.tf
@@ -52,32 +52,35 @@ data "azurerm_key_vault_secret" "sftp_user_name" {
   key_vault_id = module.opal_key_vault.key_vault_id
 }
 
-# resource "azurerm_storage_account_local_user" "sftp_local_user" {
-#   name                 = data.azurerm_key_vault_secret.sftp_user_name.value
-#   storage_account_id   = azurerm_storage_account.sftp_storage.id
-#   ssh_key_enabled      = true
-#   ssh_password_enabled = true
-#   home_directory       = "outbound"
-#
-#   permission_scope {
-#     permissions {
-#       read   = true
-#       create = true
-#       list = true
-#       write = true
-#       delete = true
-#     }
-#     service       = "blob"
-#     resource_name = azurerm_storage_container.sftp_container.name
-#   }
-#
-#   for k in data.azurerm_key_vault_secret.sftp_user_keys : {
-#        ssh_authorized_key {
-#           "description" = k.name,
-#            "key" = k.value
-#          }
-#   }
-# }
+data "azurerm_key_vault_secret" "sftp_user_key" {
+  name         = "sftp_user_key"
+  key_vault_id = module.opal_key_vault.key_vault_id
+}
+
+resource "azurerm_storage_account_local_user" "sftp_local_user" {
+  name                 = data.azurerm_key_vault_secret.sftp_user_name.value
+  storage_account_id   = azurerm_storage_account.sftp_storage.id
+  ssh_key_enabled      = true
+  ssh_password_enabled = true
+  home_directory       = "outbound"
+
+  ssh_authorized_key {
+         description = data.azurerm_key_vault_secret.sftp_user_key.name,
+         key = data.azurerm_key_vault_secret.sftp_user_key.value
+  }
+
+  permission_scope {
+    permissions {
+      read   = true
+      create = true
+      list = true
+      write = true
+      delete = true
+    }
+    service       = "blob"
+    resource_name = azurerm_storage_container.sftp_container.name
+  }
+}
 
 #TODO: Replace this API call with azurerm_storage_account_local_user resource
 resource "azapi_resource" "add_local_user" {
