c5b523ad9b5cde3613bdcdb3e1fc0ac3591d07c9
Sabah Irfan <sabah.irfan@gmail.com>
2024-04-11 16:17:33 +0100

    update azurerm version

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-# Required Providers
-terraform {
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.84.0"
-    }
-  }
-  required_version = ">= 1.1.0"
-}
-
 provider "azurerm" {
   features {}
 }
