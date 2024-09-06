ef12b06d0e60fdf18185ed4917832610c58aba0e
52960 <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>
2024-09-06 14:31:52 +0000

    Updating Terraform Formatting

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
-  db_fines_name            = "psql-${product}-fines-db"
-  db_user_name             = "psql-${var.env}-user-db"
-  db_maintenance_name      = "psql-${var.env}-maintenance-db"
-  db_port                  = 5432
+  db_fines_name       = "psql-${product}-fines-db"
+  db_user_name        = "psql-${var.env}-user-db"
+  db_maintenance_name = "psql-${var.env}-maintenance-db"
+  db_port             = 5432
 }
