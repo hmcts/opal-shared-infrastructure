9f5399e7781845e95caf70f231a3658227320fdb
benedwards <ben.edwards@cgi.com>
2026-03-13 14:47:46 +0000

    Updated storage account details

--- storage-account.tf
+++ storage-account.tf
@@ -1,8 +1,8 @@
-module "role_mapping_storage" {
+module "opal_storage" {
   source = "git@github.com:hmcts/cnp-module-storage-account?ref=4.x"
 
   env                      = var.env
-  storage_account_name     = "opalsa${var.env}"
+  storage_account_name     = "opal-${var.env}"
   resource_group_name      = azurerm_resource_group.opal_resource_group.name
   location                 = azurerm_resource_group.opal_resource_group.location
   account_kind             = "StorageV2"
@@ -11,7 +11,11 @@ module "role_mapping_storage" {
 
   containers = [
     {
-      name        = "rolemapping"
+      name        = "role_mapping"
+      access_type = "private"
+    },
+    {
+      name        = "reports"
       access_type = "private"
     }
   ]
@@ -23,11 +27,6 @@ module "role_mapping_storage" {
   common_tags  = var.common_tags
 }
 
-resource "azurerm_key_vault_secret" "role_mapping_storage_account_name" {
-  name         = "role-mapping-storage-account-name"
-  key_vault_id = module.opal_key_vault.key_vault_id
-  value        = module.role_mapping_storage.storageaccount_name
-}
 
 resource "azurerm_key_vault_secret" "role_mapping_storage_container_name" {
   name         = "role-mapping-storage-container-name"
@@ -35,20 +34,30 @@ resource "azurerm_key_vault_secret" "role_mapping_storage_container_name" {
   value        = "rolemapping"
 }
 
-resource "azurerm_key_vault_secret" "role_mapping_storage_primary_blob_endpoint" {
-  name         = "role-mapping-storage-blob-endpoint"
+resource "azurerm_key_vault_secret" "reports_storage_container_name" {
+  name         = "reports-storage-container-name"
   key_vault_id = module.opal_key_vault.key_vault_id
-  value        = module.role_mapping_storage.storageaccount_primary_blob_endpoint
+  value        = "reports"
 }
 
-resource "azurerm_key_vault_secret" "role_mapping_storage_primary_access_key" {
-  name         = "role-mapping-storage-access-key"
+resource "azurerm_key_vault_secret" "storage_primary_blob_endpoint" {
+  name         = "storage-blob-endpoint"
   key_vault_id = module.opal_key_vault.key_vault_id
-  value        = module.role_mapping_storage.storageaccount_primary_access_key
+  value        = module.opal_storage.storageaccount_primary_blob_endpoint
+}
+resource "azurerm_key_vault_secret" "storage_account_name" {
+  name         = "storage-account-name"
+  key_vault_id = module.opal_key_vault.key_vault_id
+  value        = module.opal_storage.storageaccount_name
+}
+resource "azurerm_key_vault_secret" "storage_primary_access_key" {
+  name         = "storage-access-key"
+  key_vault_id = module.opal_key_vault.key_vault_id
+  value        = module.opal_storage.storageaccount_primary_access_key
 }
 
-resource "azurerm_key_vault_secret" "role_mapping_storage_primary_connection_string" {
-  name         = "role-mapping-storage-connection-string"
+resource "azurerm_key_vault_secret" "storage_primary_connection_string" {
+  name         = "storage-connection-string"
   key_vault_id = module.opal_key_vault.key_vault_id
-  value        = module.role_mapping_storage.storageaccount_primary_connection_string
+  value        = module.opal_storage.storageaccount_primary_connection_string
 }
