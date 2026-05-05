5c591bc85372d1400b2b93660328edc11fd9451c
Ben Neill <benjamin.neill@hmcts.net>
2026-05-05 14:17:45 +0000

    Adopt legacy OPAL postgres via env-scoped imports; add consolidated nonprod postgres

---
+++ consolidated-databases.tf
@@ -0,0 +1,73 @@
+# New consolidated PostgreSQL flexible servers for non-prod cutover work.
+#
+# These are created alongside the existing service-owned PostgreSQL servers.
+# The secrets deliberately use new names and are not consumed by applications
+# until a later cutover PR.
+
+module "opal_consolidated_postgresql" {
+  count = local.consolidated_postgresql_enabled ? 1 : 0
+
+  providers = {
+    azurerm.postgres_network = azurerm
+  }
+
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+
+  name                 = "opal-consolidated-db"
+  env                  = var.env
+  product              = var.product
+  component            = var.component
+  business_area        = "sds"
+  common_tags          = var.common_tags
+  collation            = "en_US.utf8"
+  admin_user_object_id = var.jenkins_AAD_objectId
+
+  pgsql_databases = [
+    for db_name in values(local.consolidated_postgresql_databases) : {
+      name = db_name
+    }
+  ]
+
+  pgsql_server_configuration = local.consolidated_postgresql_server_configuration
+  pgsql_version              = "16"
+}
+
+resource "azurerm_key_vault_secret" "CONSOLIDATED_POSTGRES_USER" {
+  count = local.consolidated_postgresql_enabled ? 1 : 0
+
+  name         = "${var.component}-CONSOLIDATED-POSTGRES-USER"
+  value        = module.opal_consolidated_postgresql[0].username
+  key_vault_id = module.opal_key_vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "CONSOLIDATED_POSTGRES_PASS" {
+  count = local.consolidated_postgresql_enabled ? 1 : 0
+
+  name         = "${var.component}-CONSOLIDATED-POSTGRES-PASS"
+  value        = module.opal_consolidated_postgresql[0].password
+  key_vault_id = module.opal_key_vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "CONSOLIDATED_POSTGRES_HOST" {
+  count = local.consolidated_postgresql_enabled ? 1 : 0
+
+  name         = "${var.component}-CONSOLIDATED-POSTGRES-HOST"
+  value        = module.opal_consolidated_postgresql[0].fqdn
+  key_vault_id = module.opal_key_vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "CONSOLIDATED_POSTGRES_PORT" {
+  count = local.consolidated_postgresql_enabled ? 1 : 0
+
+  name         = "${var.component}-CONSOLIDATED-POSTGRES-PORT"
+  value        = local.db_port
+  key_vault_id = module.opal_key_vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "CONSOLIDATED_POSTGRES_DATABASES" {
+  for_each = local.consolidated_postgresql_enabled ? local.consolidated_postgresql_databases : {}
+
+  name         = "${var.component}-CONSOLIDATED-POSTGRES-${each.key}-DATABASE"
+  value        = each.value
+  key_vault_id = module.opal_key_vault.key_vault_id
+}
---
+++ legacy-databases.tf
@@ -0,0 +1,68 @@
+# Existing service-owned PostgreSQL flexible servers.
+#
+# These definitions mirror the database infrastructure currently held in the
+# individual service repositories. They are intended to be adopted into this
+# repo with Terraform state moves/imports so the Azure resources, data and
+# Key Vault secret names remain unchanged.
+
+module "legacy_postgresql" {
+  for_each = local.legacy_postgresql_servers
+
+  providers = {
+    azurerm.postgres_network = azurerm
+  }
+
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+
+  env                  = var.env
+  product              = var.product
+  component            = each.value.component
+  business_area        = "sds"
+  collation            = coalesce(each.value.collation, "en_GB.utf8")
+  common_tags          = var.common_tags
+  admin_user_object_id = var.jenkins_AAD_objectId
+
+  pgsql_databases            = each.value.pgsql_databases
+  pgsql_server_configuration = each.value.pgsql_server_configuration
+  pgsql_version              = each.value.pgsql_version
+}
+
+resource "azurerm_key_vault_secret" "legacy_POSTGRES_USER" {
+  for_each = local.legacy_postgresql_servers
+
+  name         = "${each.value.component}-POSTGRES-USER"
+  value        = module.legacy_postgresql[each.key].username
+  key_vault_id = module.opal_key_vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "legacy_POSTGRES_PASS" {
+  for_each = local.legacy_postgresql_servers
+
+  name         = "${each.value.component}-POSTGRES-PASS"
+  value        = module.legacy_postgresql[each.key].password
+  key_vault_id = module.opal_key_vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "legacy_POSTGRES_HOST" {
+  for_each = local.legacy_postgresql_servers
+
+  name         = "${each.value.component}-POSTGRES-HOST"
+  value        = module.legacy_postgresql[each.key].fqdn
+  key_vault_id = module.opal_key_vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "legacy_POSTGRES_PORT" {
+  for_each = local.legacy_postgresql_servers
+
+  name         = "${each.value.component}-POSTGRES-PORT"
+  value        = local.db_port
+  key_vault_id = module.opal_key_vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "legacy_POSTGRES_DATABASE" {
+  for_each = local.legacy_postgresql_servers
+
+  name         = "${each.value.component}-POSTGRES-DATABASE"
+  value        = each.value.db_name
+  key_vault_id = module.opal_key_vault.key_vault_id
+}
