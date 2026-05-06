4e98b4302acfb5ff1d9d0030cde1b4fa715a945c
Ben Neill <benjamin.neill@hmcts.net>
2026-05-06 13:42:37 +0000

    Use legacy names for new shared Postgres databases

--- locals.tf
+++ locals.tf
@@ -2,9 +2,9 @@ locals {
   db_fines_name        = "psql-${var.product}-fines-db"
   db_user_name         = "psql-${var.product}-user-db"
   db_maintenance_name  = "psql-${var.product}-maintenance-db"
-  db_logging_name      = "psql-${var.product}-logging-db"
-  db_log_audit_name    = "psql-${var.product}-log-audit-db"
-  db_file_handler_name = "psql-${var.product}-file-db"
+  db_logging_name      = "${var.product}-logging-db"
+  db_log_audit_name    = "${var.product}-log-audit-db"
+  db_file_handler_name = "${var.product}-file-db"
   db_port              = 5432
 
   legacy_postgresql_default_server_configuration = [
