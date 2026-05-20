f6f5bf6c5cc2b2104201632fdec91f2788db64c0
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2026-05-20 17:34:20 +0100

    Update service-bus.tf
    Rename queue, prefix with auto-payments

--- service-bus.tf
+++ service-bus.tf
@@ -62,16 +62,16 @@ resource "azurerm_key_vault_secret" "servicebus-queue-report-queue-name" {
   key_vault_id = module.opal_key_vault.key_vault_id
 }
 
-module "servicebus-queue-process-interface-files" {
+module "servicebus-queue-auto-payments-process-interface-files" {
   source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=4.x"
-  name                = "process-interface-files"
+  name                = "auto-payments-process-interface-files"
   namespace_name      = module.servicebus-namespace.name
   resource_group_name = azurerm_resource_group.opal_resource_group.name
   depends_on          = [module.servicebus-namespace]
 }
 
-resource "azurerm_key_vault_secret" "servicebus-queue-process-interface-files-queue-name" {
-  name         = "servicebus-process-interface-files-queue-name"
-  value        = module.servicebus-queue-process-interface-files.name
+resource "azurerm_key_vault_secret" "servicebus-queue-auto-payments-process-interface-files-queue-name" {
+  name         = "servicebus-auto-payments-process-interface-files-queue-name"
+  value        = module.servicebus-queue-auto-payments-process-interface-files.name
   key_vault_id = module.opal_key_vault.key_vault_id
 }
