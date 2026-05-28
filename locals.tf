19a1f80df343719803a7759145ca95f113568d0e
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2026-05-28 11:29:13 +0100

    Fixed issue with secrets on consolidated environments (#90)
    * Fixed issue with secrets on consolidated environments
    
    * Fixed issue with component
    
    * Fixed issue with component

--- locals.tf
+++ locals.tf
@@ -123,7 +123,10 @@ locals {
 
   consolidated_postgresql_databases = {
     for _, v in local.legacy_postgresql_all_servers :
-    upper(replace(replace(replace(v.db_name, "opal-", ""), "-db", ""), "-", "_")) => v.db_name
+    upper(replace(replace(replace(v.db_name, "opal-", ""), "-db", ""), "-", "_")) => {
+      component = v.component
+      db_name   = v.db_name
+    }
   }
 
   postgresql_all_enabed_servers_none_consolidated = {
@@ -131,10 +134,6 @@ locals {
     name => config
     if !local.consolidated_postgresql_enabled && contains(config.enabled_envs, var.env)
   }
-
-  service_keyvault_databases_prefix = local.consolidated_postgresql_enabled ? {
-    LOGGING = "logging-service"
-  } : {}
 
   consolidated_postgresql_server_configuration = [
     {
