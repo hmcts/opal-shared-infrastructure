a0b74cca932ba03ef9461d494fa29bbfaa466b0c
Ben Neill <benjamin.neill@hmcts.net>
2026-06-23 10:14:03 +0000

    Fix managed Redis private endpoint subnet

--- redis.tf
+++ redis.tf
@@ -1,9 +1,3 @@
-data "azurerm_subnet" "redis_private_endpoint" {
-  name                 = "core-infra-subnet-2-${var.env}"
-  resource_group_name  = "core-infra-${var.env}"
-  virtual_network_name = "core-infra-vnet-${var.env}"
-}
-
 module "opal_managed_redis" {
   source = "git@github.com:hmcts/terraform-module-azure-managed-redis?ref=main"
 
@@ -19,8 +13,8 @@ module "opal_managed_redis" {
   # Networking:
   public_network_access   = "Disabled"
   create_private_endpoint = true
-  subnet_id               = data.azurerm_subnet.redis_private_endpoint.id
-  private_dns_zone_ids    = ["/subscriptions/${var.private_dns_subscription_id}/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/privatelink.redis.azure.net"]
+  subnet_id               = data.azurerm_subnet.private_endpoints.id
+  private_dns_zone_ids    = ["/subscriptions/${var.private_dns_subscription_id}/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/privatelink.redisenterprise.cache.azure.net"]
 
   access_keys_authentication_enabled = true
 
