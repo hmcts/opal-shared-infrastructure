935065d1433d25552b0783cd1e48fbac0618991f
benedwards <ben.edwards@cgi.com>
2026-06-26 17:18:58 +0100

    Added name for managed redis

--- redis_managed.tf
+++ redis_managed.tf
@@ -6,7 +6,7 @@ data "azurerm_subnet" "redis_private_endpoint" {
 
 module "opal_managed_redis" {
   source = "git@github.com:hmcts/terraform-module-azure-managed-redis?ref=main"
-
+  name = "${var.product}-${var.env}"
   product     = var.product
   component   = var.component
   env         = var.env
