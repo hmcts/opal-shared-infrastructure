3f38226acbf82873176bedf0a2d0366c4d8f0da2
benedwards <ben.edwards@cgi.com>
2026-07-20 13:38:11 +0100

    Jenkins Migraiton

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ provider "random" {}
 data "azurerm_user_assigned_identity" "jenkins" {
   name                = "jenkins-${var.env}-mi"
   resource_group_name = "managed-identities-${var.env}-rg"
+}
 
 data "azurerm_subnet" "private_endpoints" {
   resource_group_name  = local.private_endpoint_rg_name
