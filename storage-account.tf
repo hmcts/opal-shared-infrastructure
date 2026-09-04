5af96a836b91afff8ba385469b504c0e933999be
jordan-muscott-cgi <288038593+jordan-muscott-cgi@users.noreply.github.com>
2026-09-04 14:04:35 +0100

    Add all natwest details (#158)
    * add all natwest deails
    
    * fix natwest container name
    
    * removed unncessary section

--- storage-account.tf
+++ storage-account.tf
@@ -86,6 +86,10 @@ module "opal_file_handler_service_file_store" {
     {
       name        = "allpay"
       access_type = "private"
+    },
+    {
+      name        = "natwest"
+      access_type = "private"
     }
   ]
 
