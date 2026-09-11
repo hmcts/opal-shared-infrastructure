2b086a1e29e983a96f3e454e863c73a5d16b6025
Sam Naylor <sam.naylor@hmcts.net>
2026-09-11 11:14:43 +0100

    PO-6437 - jacobs transfer file infra setup (#171)
    * jacobs transfer file infra setup
    
    * Updating Terraform Formatting
    
    ---------
    
    Co-authored-by: 52960 <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>
    Co-authored-by: Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>

--- storage-account.tf
+++ storage-account.tf
@@ -102,6 +102,10 @@ module "opal_file_handler_service_file_store" {
     {
       name        = "barclaycard"
       access_type = "private"
+    },
+    {
+      name        = "jacobs"
+      access_type = "private"
     }
   ]
 
